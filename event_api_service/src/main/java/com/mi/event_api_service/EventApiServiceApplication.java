9c4114bed823900d5ad589ba0ac4b875b757d01d
Robin Baby <robin.baby@qburst.com>
2026-04-21 20:53:16 +0530

    initialize event_api_service with Maven setup and Spring Boot configuration

--- event_api_service/src/main/java/com/mi/event_api_service/EventApiServiceApplication.java
+++ event_api_service/src/main/java/com/mi/event_api_service/EventApiServiceApplication.java
@@ -1,13 +1,13 @@
-package com.mi.parent;
+package com.mi.event_api_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParentApplication {
+public class EventApiServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParentApplication.class, args);
+		SpringApplication.run(EventApiServiceApplication.class, args);
 	}
 
 }
--- event_api_service/src/test/java/com/mi/event_api_service/EventApiServiceApplicationTests.java
+++ event_api_service/src/test/java/com/mi/event_api_service/EventApiServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mi.parent;
+package com.mi.event_api_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ParentApplicationTests {
+class EventApiServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
