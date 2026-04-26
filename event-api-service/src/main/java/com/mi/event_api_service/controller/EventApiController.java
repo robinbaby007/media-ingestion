ec1cb1acb15249c031e2714d5feb81ca6a4a921f
Robin Baby <robin.baby@qburst.com>
2026-04-26 21:43:24 +0530

    Add EventProducer to EventApiController for publishing media events

--- event-api-service/src/main/java/com/mi/event_api_service/controller/EventApiController.java
+++ event-api-service/src/main/java/com/mi/event_api_service/controller/EventApiController.java
@@ -1,5 +1,6 @@
 package com.mi.event_api_service.controller;
 
+import com.mi.event_api_service.kafka.EventProducer;
 import com.mi.event_api_service.model.MediaEventRequest;
 import com.mi.event_api_service.service.EventMediaService;
 import jakarta.validation.Valid;
@@ -16,6 +17,7 @@ import java.util.Map;
 public class EventApiController {
 
     private final EventMediaService eventMediaService;
+    private final EventProducer eventProducer;
 
     @PostMapping
     public ResponseEntity<String> sentMediaEvent(
@@ -27,6 +29,7 @@ public class EventApiController {
 
         Boolean isEventNotExists = eventMediaService.sentMediaEvent(mediaEventRequest, idempotencyKey);
         if (isEventNotExists) {
+            eventProducer.publishOrderCreatedEvent(mediaEventRequest);
             return ResponseEntity.status(HttpStatus.CREATED).body("Event received successfully");
         } else {
             return ResponseEntity.status(HttpStatus.OK).body("Duplicate event detected. This event has already been processed.");
