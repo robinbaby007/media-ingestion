d0cd0c3f54cba85daa3c2464903f94a535c814cf
Robin Baby <robin.baby@qburst.com>
2026-04-29 11:29:50 +0530

    Add Eureka client configuration and dependencies for service discovery

--- event-api-service/src/main/java/com/mi/event_api_service/EventApiServiceApplication.java
+++ event-api-service/src/main/java/com/mi/event_api_service/EventApiServiceApplication.java
@@ -2,8 +2,10 @@ package com.mi.event_api_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventApiServiceApplication {
 
 	public static void main(String[] args) {
