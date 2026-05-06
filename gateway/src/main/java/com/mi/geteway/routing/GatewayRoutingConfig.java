7b79d23717974ed6d7c7707edbf16ef6c6cd62ba
Robin Baby <robin.baby@qburst.com>
2026-05-06 16:55:29 +0530

    Implement Redis rate limiting and configuration: add Redis properties to application.yml, update docker-compose.yml for Redis service, and enhance GatewayRoutingConfig with rate limiter functionality.

--- gateway/src/main/java/com/mi/geteway/routing/GatewayRoutingConfig.java
+++ gateway/src/main/java/com/mi/geteway/routing/GatewayRoutingConfig.java
@@ -1,9 +1,14 @@
 package com.mi.geteway.routing;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 public class GatewayRoutingConfig {
@@ -13,10 +18,14 @@ public class GatewayRoutingConfig {
                 .route("event-api-service", r -> r
                         // Matches both /api/events and /api/events/anything
                         .path("/api/events", "/api/events/**")
+                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(apiEventsRateLimiter())
+                                .setKeyResolver(ipKeyResolver())))
                         // Forwards to the load balancer
                         .uri("lb://EVENT-API-SERVICE"))
                 .route("event-ingestion-service", r -> r
                         .path("/api/ingestion", "/api/ingestion/**")
+                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(apiEventsRateLimiter())
+                                .setKeyResolver(ipKeyResolver())))
                         .uri("lb://EVENT-INGESTION-SERVICE")
                 )
                 .route("user-service", r -> r
@@ -25,4 +34,23 @@ public class GatewayRoutingConfig {
                 )
                 .build();
     }
+
+    // Define the rate limits for this specific route
+    @Bean
+    public RedisRateLimiter apiEventsRateLimiter() {
+        // replenishRates - number of tokens added to the bucket every second
+        // burstCapacity - maximum number of tokens the bucket can hold (the burst capacity)
+        // requestedTokens - number of tokens required for each request (default is 1)
+        return new RedisRateLimiter(1, 2, 1);
+    }
+
+    // Rate limits based on the client's IP address
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress()
+        );
+    }
+
+
 }
